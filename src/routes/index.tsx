406a321cdaed164ee6b263369213820238762913
hyoaru <jjcabreraaaa@gmail.com>
2025-08-05 20:21:39 +0800

    [add] react flow

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,66 @@
-import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
+
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  Background,
+  Controls,
+  MiniMap,
+  BackgroundVariant,
+  type Node,
+  type Edge,
+  type OnNodesChange,
+  type OnEdgesChange,
+  type OnConnect,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
+const initialNodes: Node[] = [
+  { id: "n1", position: { x: 0, y: 0 }, data: { label: "Node 1" } },
+  { id: "n2", position: { x: 0, y: 100 }, data: { label: "Node 2" } },
+];
+const initialEdges: Edge[] = [
+  { id: "n1-n2", source: "n1", target: "n2", animated: true },
+];
+
 function RouteComponent() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange: OnNodesChange = useCallback((changes) => {
+    setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot));
+  }, []);
+
+  const onEdgesChange: OnEdgesChange = useCallback((changes) => {
+    setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot));
+  }, []);
+
+  const onConnect: OnConnect = useCallback((params) => {
+    setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot));
+  }, []);
   return (
     <>
-      <Button>Hello</Button>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          fitView
+        >
+          <Controls />
+          <MiniMap />
+          <Background variant={BackgroundVariant.Dots} gap={10} size={0.8} />
+        </ReactFlow>
+      </div>
     </>
   );
 }
