d3fea0eca7aaa25dd2ad8052dd1a97ca8cccb24b
hyoaru <jjcabreraaaa@gmail.com>
2025-08-16 15:25:23 +0800

    Add on connect listener

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,22 @@
 import { CanvasControlBar } from "@/components/CanvasControlBar";
 import TextNode from "@/components/TextNode";
+import { useGraphLayout } from "@/hooks/useGraphLayout";
 import { createFileRoute } from "@tanstack/react-router";
 import {
+  addEdge,
   Background,
   BackgroundVariant,
-  Controls,
   ReactFlow,
   useEdgesState,
   useNodesState,
+  type Connection,
+  type Edge,
   type Node,
+  type ReactFlowInstance,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import { nanoid } from "nanoid";
+import { useCallback, useRef } from "react";
 
 export const Route = createFileRoute("/")({ component: RouteComponent });
 
@@ -26,8 +31,40 @@ const initialNodes: Node[] = [
 ];
 
 function RouteComponent() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
-  const [edges, , onEdgesChange] = useEdgesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([] as Edge[]);
+  const reactFlow = useRef<ReactFlowInstance | null>(null);
+  const graphLayout = useGraphLayout();
+
+  const onConnect = useCallback(
+    (params: Connection) => {
+      const updatedEdges = addEdge({ ...params, animated: true }, edges);
+
+      const updateLayout = graphLayout.run({
+        nodes: nodes,
+        edges: updatedEdges,
+      });
+
+      setNodes(updateLayout.nodes);
+      setEdges(updateLayout.edges);
+
+      if (!reactFlow.current) throw new Error("Instance does not exist");
+
+      const targetNode = updateLayout.nodes.find((n) => n.id == params.target);
+      if (!targetNode) throw new Error("Target node does not exist");
+
+      reactFlow.current.setCenter(
+        targetNode.position.x,
+        targetNode.position.y,
+        {
+          zoom: 1,
+          duration: 100,
+          interpolate: "smooth",
+        },
+      );
+    },
+    [setEdges, setNodes, edges, nodes, graphLayout],
+  );
 
   return (
     <>
@@ -36,13 +73,17 @@ function RouteComponent() {
           <CanvasControlBar />
         </div>
         <ReactFlow
+          onInit={(instance: ReactFlowInstance) =>
+            (reactFlow.current = instance)
+          }
           nodes={nodes}
           edges={edges}
           nodesDraggable={false}
-          nodesConnectable={false}
+          nodesConnectable={true}
           nodeTypes={{ text: TextNode }}
           onNodesChange={onNodesChange}
           onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
           proOptions={{ hideAttribution: true }}
           fitView
         >
